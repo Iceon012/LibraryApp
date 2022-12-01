b9e5a6ed2492193a05cd670960a54fc0cf46fa10
Iceon012 <a.ayson12345.com>
2022-12-01 16:01:05 +0800

    Update Edit Components

--- src/app/viewstudents/viewstudents.component.ts
+++ src/app/viewstudents/viewstudents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LibrarylogService } from '../librarylog.service';
 
@@ -8,6 +9,12 @@ import { LibrarylogService } from '../librarylog.service';
   styleUrls: ['./viewstudents.component.css']
 })
 export class ViewstudentsComponent {
+
+  updateStud = new FormGroup({
+    ID: new FormControl(null),
+    Name: new FormControl(null),
+   Course: new FormControl(null),
+  })
 
   constructor(private activeroute: ActivatedRoute,
     private post: LibrarylogService, private route: Router) { }
@@ -23,9 +30,14 @@ export class ViewstudentsComponent {
 
     this.post.StudLog(this.studid).subscribe((result:any)=>{
       this.studid = result;
-
       console.log(this.studid)
+
+      this.updateStud.controls['ID'].setValue(this.studid[0].stud_id);
+      this.updateStud.controls['Name'].setValue(this.studid[0].Name);
+      this.updateStud.controls['Course'].setValue(this.studid[0].course);
     })
+
+    
 
   }
   
